5c70bf903faeccb6d91aeabcc3f1349b9ab52984
joloe <johannes.loevenich@webusers.de>
2014-06-10 16:19:32 +0200

    Changed to VectorXd format for mu and sigma.

--- analysis/gameSetup.cpp
+++ analysis/gameSetup.cpp
@@ -5,52 +5,59 @@
 #include <vector>
 #include "gameSetup.h"
 
-/// Generate gameSetup from number of Players.
-using namespace std;
 
-gameSetup::gameSetup(int p, int t)
-: numberPlayers(p), numberTeams(t), beta(4.166666666666667), tau(0.08333333333333334) 
-{
-  /// Set mu and sigma to default trueskill values. 
-  this->mu =  vector<double>(this->numberPlayers,25.000);
-  this->sigma = vector<double>(this->numberPlayers,8.333);
-  this->teams = vector<int> (t);
+using namespace std;
+ using namespace Eigen;
+
+/// Players holds the player indices sorted by teams. Teams holds the number of players in team i. 
+gameSetup::gameSetup(vector<int> players,vector<int> team)
+: sortedPlayers(players),teams(team), numberPlayers(players.size()), numberTeams(team.size()), beta(4.166666666666667), tau(0.08333333333333334){
+  this->mu =  VectorXd(this->numberPlayers);
+  for(int i = 0; i< numberPlayers;i++){
+    this->mu[i] = 25.000; 
+  }
+  this->sigma = VectorXd(this->numberPlayers);
+   for(int i = 0; i< numberPlayers;i++){
+    this->sigma[i] = 8.333; 
+  }
 }
 
-/// Add player to team. 
-void gameSetup::addPlayerToTeam(int teamIndex){
-  this->numberPlayers++;
-  this->teams[teamIndex-1]++;
-  vector<int>::iterator iter;
-  int sum = 0;
-  for(iter = teams.begin(); iter != teams.end(); ++iter){
-    sum = sum + (*iter);
-  }
-  iter = sortedPlayers.begin() + sum;
-  this->sortedPlayers.insert(iter,this->numberPlayers);
+/// Resizes a Team.
+void gameSetup::resizeTeam(int teamIndex, int size){
+  this->teams.at(teamIndex) = size;
 }
 
 void gameSetup::printSetup(){
+  cout << "-- This is GameSetup for your created Game: -- \n" << " - Constants are set by recommended default values of Trueskill - \n" << endl;
   cout << "teamVec: \n" << endl;
   
   for(int i = 0; i< this->numberTeams;i++){
     cout << "[" << this->teams.at(i) << "]" << endl;
   }
-  cout << " \n playerVec: \n" << endl;
+  cout << " \nplayerVec: \n" << endl;
   
   for(int i = 0; i< this->numberPlayers;i++){
     cout << "[" << this->sortedPlayers.at(i) << "]" << endl;
   }
   
-  cout << "\nMu: " << this->mu.at(0) << "\n" << endl;
+  cout << "\nMu: " << this->mu[0] << endl;
   
-  cout << "Sigma: " << this->sigma.at(0) << "\n"  << endl;
+  cout << "Sigma: " << this->sigma[0]  << endl;
   
-  cout << "Beta: " << this->beta << "\n"  << endl;
+  cout << "Beta: " << this->beta  << endl;
  
-  cout << "Tau: " << this->tau << "\n" << endl;
+  cout << "Tau: " << this->tau << endl;
 }
 
+/// Test environment for gameSetup. 
 int main(void){
-  
+  vector<int> p;
+  p.push_back(1);
+   p.push_back(2);
+    p.push_back(3);
+  vector<int> t;
+  t.push_back(1);
+  t.push_back(2);
+  gameSetup S1(p,t);
+  S1.printSetup();
 }
--- analysis/gameSetup.h
+++ analysis/gameSetup.h
@@ -6,27 +6,31 @@
 
 
 using namespace std;
+using namespace Eigen;
 
 class gameSetup{
+  
+public:
   int numberPlayers;
   int numberTeams;
   /// Holds player indices sorted by teams.
   vector<int> sortedPlayers;
   /// Holds numbers of players sorted by teams.
   vector<int> teams;
-  vector<double> mu;
-  vector<double> sigma;
+  VectorXd mu;
+  VectorXd sigma;
   double beta;
   double tau;
 
-public:
   /// Default Constructor for gameSetup;
   gameSetup();
-  /// Generate gameSetup from number of Players.
-  gameSetup(int p, int t);
   
-  /// Add player to team. 
-  void addPlayerToTeam(int teamIndex);
+  /// Generate gameSetup from number of Players.
+  gameSetup(vector<int> players,vector<int> team);
+ 
+  /// Resizes a team.
+  void resizeTeam(int teamIndex, int size);
+
   
   /// Prints Setup. 
   void printSetup();
