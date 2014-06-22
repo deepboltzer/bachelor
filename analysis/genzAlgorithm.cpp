80c8174e328255a9d543c3a7e50495a309a02d11
joloe <johannes.loevenich@webusers.de>
2014-06-22 20:15:11 +0200

    Update of GenzAlgorithm:
    * Changed random sampler.
    * Changed from double structure of d,e,f to vector structure.
    * Several bugfixes

--- analysis/genzAlgorithm.cpp
+++ analysis/genzAlgorithm.cpp
@@ -6,65 +6,93 @@
 #include <vector>
 #include "gameSetup.h"
 #include <boost/math/distributions/normal.hpp>
-#include "../gaussians/gaussian.h"
 #include "../random/randomGenerator.h"
 
+
 using namespace std;
+
+/// Set values of infinity and -infinity:
 double ninf  = -numeric_limits<double>::infinity();
 double inf  = numeric_limits<double>::infinity();
+
+/// TODO: Evaluate Cholesky decomp, Find Error.
+
 void genzAlgorithm(VectorXd a, VectorXd b, MatrixXd covar) 
 {
-  boost::math::normal norm;
+  int N, m;
+  
+  double delta = 0;
+  double error = 1000;
+  double sum = 0; 
+  double varsum, itsum;
+  
+  VectorXd d(m);
+  VectorXd e(m); 
+  VectorXd f(m);
+  VectorXd y(m); 
+  VectorXd w(m);
+  
   MatrixXd C(3,3); 
+  
+  boost::math::normal norm;
+  
+  N = 0; varsum = 0; itsum = 0; m = 3; 
+  
   C << 1.0 , 0., 0. ,
   3./5., 4./5., 0.,
   1./3., 2./3.,2./3.;
-  int N, m;
-  double varsum, itsum;
-  N = 0; varsum = 0; itsum = 0; m = 3; 
-  double d,e;
-  if(a[0] == ninf) d = 0;
-  else d = cdf(norm,(a[1]/C(1,1)));
-  if(b[0] = inf) e = 1; 
-  else e = cdf(norm,(b[0]/C(1,1)));
-  double f = e-d; 
-  cout << "f: " << f << endl;
-  double delta = 0;
-  double error = 1000;
-  VectorXd y(m);
-  double sum = 0; 
-  cout << d << "	"<< e << endl;
-  randomGenerator rg(m,1);
-  rg.generateRandomSample();
-  VectorXd w = rg.getSample();
-  cout << w << endl;
-  while(N<=1000){
-  for(int i = 1; i<m; i++){
-    w[0] = 0.47839;
-    sum = 0; 
-    cout << "w: "<< w[i-1] << "d + w[i-1]*(e-d): " << d + w[i-1]*(e-d) << endl;
-    y[i-1] = quantile(norm,(d + w[i-1]*(e-d))); 
-    cout << "y: " << y[i-1] << endl;
-    for(int j = 0; j<= i-1;j++){
-      sum += C(i,j)*y[j];
+ 
+
+  if(a[0] == ninf) d[0] = 0;
+  else d[0] = cdf(norm,(a[1]/C(1,1)));
+  
+  if(b[0] = inf) e[0] = 1; 
+  else e[0] = cdf(norm,(b[0]/C(1,1)));
+  
+  f[0] = e[0]-d[0]; 
+  
+  
+  while(N<=10000){
+    for(int i = 1; i<m; i++){
+      
+      /// Generate random sample:
+      for(int k = 0; k<m;k++){
+	w[k] = ((double) rand() / (RAND_MAX));
+      }
+    
+      sum = 0; 
+      
+      /// Evaluate Quantile Function:
+      y[i-1] = quantile(norm,(d[i-1] + w[i-1]*(e[i-1]-d[i-1]))); 
+      
+      /// Compute sum for evaluation of d and e:
+      for(int j = 0; j<= i-1;j++){
+	sum += C(i,j)*y[j];
+      }
+  
+      /// Set d:
+      if(a[i] == ninf) d[i] = 0;
+      else d[i] = cdf(norm,((a[i] -sum)/C(i,i))); 
+      
+      /// Set e:
+      if(b[i] == inf) e[i] = 1; 
+      else e[i] = cdf(norm,((b[i] -sum)/C(i,i)));
+      
+      /// Set f:
+      f[i] = (e[i] - d[i])*f[i-1];
+      
     }
-    cout << "sum:" << sum << endl;
-    if(a[i] == ninf) d = 0;
-    else d = cdf(norm,((a[i] -sum)/C(i,i))); 
-    if(b[i] == inf) e = 1; 
-    else e = cdf(norm,((b[i] -sum)/C(i,i)));
-    cout << d << "	"<< e << endl;
-    f = (e - d)*f;
-    cout << "(b[i] -sum)/C(i,i)): " << (b[i] -sum)<< endl;
-  }
-  N = N+1;
-  delta = (f - itsum)/double(N);
-  itsum = itsum + delta;
-  varsum = double(N-2)*varsum/double(N) + delta*delta;
-  error = 2.5*sqrt(varsum);
-  cout << "intsum: " << itsum << endl;
-  cout << "error: " << error << endl;
-  }
+  
+    N = N+1;
+    
+    /// Set delta:
+    delta = (f[m-1] - itsum)/double(N);
+    /// Set itsum, varsum and delta:
+    itsum = itsum + delta;
+    varsum = double(N-2)*varsum/double(N) + delta*delta;
+    error = 2.5*sqrt(varsum);
+    cout << "intsum: " << itsum << endl;
+    cout << "error: " << error << endl;
 }
 int main(void){
   VectorXd a(3);a << ninf,ninf,ninf;
